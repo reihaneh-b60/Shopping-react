4f011f649ff6627bebabeb6c00828161635bf13d
Reihaneh bazoubandi <reihaneh.bazoubandi@gmail.com>
2022-09-30 09:55:45 +0330

    shop commit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import { useState } from 'react'
-import { Button } from 'react-bootstrap'
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './component/basalam/HomePage'
-import Login from './component/pages/loginpage/login'
-import Test from './component/pages/testPage/test'
-import Err from './component/Router_react/404/404'
-import Home from './component/Router_react/HomePage/HomePage'
-import Product from './component/Router_react/Product/Product'
 
 function App() {
   
